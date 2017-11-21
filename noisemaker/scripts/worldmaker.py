89cfc4246b9b1ab752ed4f38cd9805b4aab00de4
Alex Ayars <aayars@gmail.com>
2017-11-20 21:54:35 -0800

    break worldmaker layers into functions

--- noisemaker/scripts/worldmaker.py
+++ noisemaker/scripts/worldmaker.py
@@ -9,6 +9,25 @@ import noisemaker.effects as effects
 import noisemaker.generators as generators
 import noisemaker.recipes as recipes
 
+SMALL_X = 512
+SMALL_Y = 512
+
+LARGE_X = 1024
+LARGE_Y = 1024
+
+FREQ = 4
+SATURATION = .333
+OCTAVES = 6
+
+CONTROL_FILENAME = "worldmaker-control.png"
+LOW_FILENAME = "worldmaker-lowland.png"
+MID_FILENAME = "worldmaker-midland.png"
+HIGH_FILENAME = "worldmaker-highland.png"
+
+BLENDED_FILENAME = "worldmaker-blended.png"
+
+FINAL_FILENAME = "worldmaker.png"
+
 
 @click.group()
 def main():
@@ -16,120 +35,144 @@ def main():
 
 
 @main.command()
-def land():
-    pre_shape = [512, 512, 3]
-    post_shape = [1024, 1024, 3]
+def lowland():
+    shape = [LARGE_Y, LARGE_X, 3]
 
-    freq = 3
-    saturation = .333
-    octaves = 8
-
-    water_kwargs = {
-        "distrib": "uniform",
-        "freq": 256,
-        "hue_range": .05 + random.random() * .05,
-        "hue_rotation": .5125 + random.random() * .025,
-        "reflect_range": .333 + random.random() * .333,
-    }
-
-    mid_kwargs = {
+    kwargs = {
         "deriv": 1,
         "deriv_alpha": .25,
-        "freq": freq,
+        "freq": FREQ,
         "hue_range": .125 + random.random() * .125,
         "hue_rotation": .925 + random.random() * .05,
         "lattice_drift": 1,
-        "octaves": octaves,
+        "octaves": OCTAVES,
         "point_freq": 10,
-        "saturation": saturation,
+        "saturation": SATURATION,
         "voronoi_alpha": .333,
         "voronoi_nth": 0,
         "with_voronoi": 2,
     }
 
-    mid2_kwargs = {
+    tensor = generators.multires(shape=shape, **kwargs)
+
+    with tf.Session().as_default():
+        save(tensor, LOW_FILENAME)
+
+
+@main.command()
+def midland():
+    shape = [LARGE_Y, LARGE_X, 3]
+
+    kwargs = {
         "deriv": 1,
         "deriv_alpha": .25,
-        "freq": freq * 2,
+        "freq": FREQ * 2,
         "hue_range": .125 + random.random() * .125,
         "hue_rotation": .925 + random.random() * .05,
-        "octaves": octaves,
+        "octaves": OCTAVES,
         "point_freq": 10,
-        "saturation": saturation,
+        "saturation": SATURATION,
         "voronoi_refract": .5,
         "voronoi_alpha": .333,
         "voronoi_nth": 1,
         "with_voronoi": 6,
     }
 
-    high_kwargs = {
+    tensor = generators.multires(shape=shape, **kwargs)
+
+    with tf.Session().as_default():
+        save(tensor, MID_FILENAME)
+
+
+@main.command()
+def highland():
+    shape = [LARGE_Y, LARGE_X, 3]
+
+    kwargs = {
         "deriv": 1,
         "deriv_alpha": 0.25 + random.random() * .125,
-        "freq": freq * 3,
+        "freq": FREQ * 3,
         "hue_range": .125 + random.random() * .125,
         "hue_rotation": .925 + random.random() * .05,
-        "octaves": octaves,
+        "octaves": OCTAVES,
         "point_freq": 10,
         "ridges": True,
-        "saturation": saturation,
+        "saturation": SATURATION,
         "voronoi_alpha": .333,
         "voronoi_nth": 1,
         "with_voronoi": 2,
     }
 
+    tensor = generators.multires(shape=shape, **kwargs)
+
+    with tf.Session().as_default():
+        save(tensor, HIGH_FILENAME)
+
+
+@main.command("control")
+def _control():
+    shape = [LARGE_Y, LARGE_X, 3]
+
+    control = generators.multires(shape=shape, freq=4, octaves=OCTAVES)
+
+    control = effects.value_map(control, shape, keep_dims=True)
+
+    with tf.Session().as_default():
+        save(control, CONTROL_FILENAME)
+
+
+@main.command()
+def blended():
+    shape = [LARGE_Y, LARGE_X, 3]
+
     erode_kwargs = {
         "alpha": .05,
-        "density": 250,
-        "iterations": 250,
+        "density": 500,
+        "iterations": 125,
         "inverse": True,
         "xy_blend": .25,
     }
 
-    control = generators.multires(shape=pre_shape, freq=4, octaves=octaves)
+    control = tf.image.convert_image_dtype(load(CONTROL_FILENAME), tf.float32)
 
-    water = generators.multires(shape=pre_shape, **water_kwargs)
-    water = effects.blend(water, tf.ones(pre_shape) * tf.stack([.05, .2, .333]), .875)
+    water = tf.ones(shape) * tf.stack([.05, .2, .333])
     water = effects.blend(water, control * 4.0, .125)
 
-    mid = generators.multires(shape=pre_shape, **mid_kwargs)
-    mid2 = generators.multires(shape=pre_shape, **mid2_kwargs)
-    high = generators.multires(shape=pre_shape, **high_kwargs)
+    low = tf.image.convert_image_dtype(load(LOW_FILENAME), tf.float32)
+    mid = tf.image.convert_image_dtype(load(MID_FILENAME), tf.float32)
+    high = tf.image.convert_image_dtype(load(HIGH_FILENAME), tf.float32)
 
-    values = effects.value_map(control, pre_shape, keep_dims=True)
+    blend_control = generators.multires(shape=shape, freq=FREQ, ridges=True, octaves=1, post_refract_range=2.5)
+    blend_control = 1.0 - effects.value_map(blend_control, shape, keep_dims=True) * .5
 
-    blend_control = generators.multires(shape=pre_shape, freq=freq, ridges=True, octaves=1, post_refract_range=2.5)
-    blend_control = 1.0 - effects.value_map(blend_control, pre_shape, keep_dims=True) * .5
+    combined_land = effects.blend_layers(control, shape, blend_control, control * 2, low, mid, high)
+    combined_land = effects.erode(combined_land, shape, **erode_kwargs)
+    combined_land = effects.shadow(combined_land, shape, 1.0)
 
-    combined_land = effects.blend_layers(values, pre_shape, blend_control, control * 2, mid, mid2, high)
-    combined_land = effects.erode(combined_land, pre_shape, **erode_kwargs)
-    combined_land = effects.shadow(combined_land, pre_shape, 1.0)
-
-    combined = effects.blend_layers(effects.resample(values, pre_shape), pre_shape, .01, water, combined_land, combined_land, combined_land)
+    combined = effects.blend_layers(control, shape, .01, water, combined_land, combined_land, combined_land)
     combined = effects.blend(combined_land, combined, .625)
 
-    combined = effects.resample(combined, post_shape)
-
-    combined = effects.bloom(combined, post_shape, .333)
-    combined = recipes.dither(combined, post_shape, .1)
+    combined = effects.bloom(combined, shape, .333)
+    combined = recipes.dither(combined, shape, .1)
 
     combined = tf.image.adjust_saturation(combined, .75)
 
     with tf.Session().as_default():
-        save(combined, 'world.png')
+        save(combined, BLENDED_FILENAME)
 
 
 @main.command()
-@click.argument('input_filename', default="world.png")
+@click.argument('input_filename', default=BLENDED_FILENAME)
 def clouds(input_filename):
     tensor = tf.image.convert_image_dtype(load(input_filename), tf.float32)
 
-    pre_shape = [512, 512, 1]
-    post_shape = [1024, 1024, 1]
+    pre_shape = [SMALL_Y, SMALL_X, 1]
+    post_shape = [LARGE_Y, LARGE_X, 1]
 
     control_kwargs = {
-        "freq": 8,
+        "freq": FREQ * 2,
         "lattice_drift": 1,
-        "octaves": 8,
+        "octaves": OCTAVES,
         "ridges": True,
         "shape": pre_shape,
         "warp_freq": 3,
@@ -137,7 +180,7 @@ def clouds(input_filename):
         "warp_octaves": 2,
     }
 
-    control = generators.multires(**control_kwargs) * 2.0
+    control = generators.multires(**control_kwargs)
 
     layer_0 = tf.ones(pre_shape)
     layer_1 = tf.zeros(pre_shape)
@@ -149,17 +192,17 @@ def clouds(input_filename):
     shadow = effects.convolve(effects.ConvKernel.blur, shadow, pre_shape)
     shadow = effects.convolve(effects.ConvKernel.blur, shadow, pre_shape)
     shadow = effects.convolve(effects.ConvKernel.blur, shadow, pre_shape)
-    shadow = effects.resample(shadow, post_shape)
 
+    shadow = effects.resample(shadow, post_shape)
     combined = effects.resample(combined, post_shape)
 
     tensor = effects.blend(tensor, tf.zeros(post_shape), shadow * .5)
     tensor = effects.blend(tensor, tf.ones(post_shape), combined)
 
-    post_shape = [1024, 1024, 3]
+    post_shape = [LARGE_Y, LARGE_X, 3]
 
     tensor = effects.bloom(tensor, post_shape, .333)
     tensor = recipes.dither(tensor, post_shape, .1)
 
     with tf.Session().as_default():
-        save(tensor, 'clouds.png')
+        save(tensor, FINAL_FILENAME)
