bb1f99d620a9677b0f8e103325aa92b1d972d956
Alex Ayars <aayars@gmail.com>
2018-12-01 22:01:25 +0000

    add pre-commit hook

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='noisemaker',
-      version='0.1.0',
+      version='0.1.1',
       description='Generates procedural noise with Python 3 and TensorFlow',
       author='Alex Ayars',
       author_email='aayars@gmail.com',
