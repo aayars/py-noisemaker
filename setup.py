22c8fbe2027947bcf6eb45dd452f35a33322b48c
Alex Ayars <aayars@gmail.com>
2018-12-01 23:35:48 +0000

    Empty commit for build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='noisemaker',
-      version='0.1.5',
+      version='0.1.6',
       description='Generates procedural noise with Python 3 and TensorFlow',
       author='Alex Ayars',
       author_email='aayars@gmail.com',
