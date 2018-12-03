bced501473a44045c5253e46efa8c292972e7bd7
Alex Ayars <aayars@gmail.com>
2018-12-03 08:17:22 +0000

    quiet commands

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='noisemaker',
-      version='0.1.16',
+      version='0.1.17',
       description='Generates procedural noise with Python 3 and TensorFlow',
       author='Alex Ayars',
       author_email='aayars@gmail.com',
