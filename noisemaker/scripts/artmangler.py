252acd76a663526052b7bfbbe12541044796316e
Alex Ayars <aayars@gmail.com>
2018-12-09 23:53:48 +0000

    use effects.square_crop_and_resize

--- noisemaker/scripts/artmangler.py
+++ noisemaker/scripts/artmangler.py
@@ -23,42 +23,16 @@ import noisemaker.recipes as recipes
 def main(ctx, seed, name, preset_name, input_filename):
     generators.set_seed(seed)
 
-    tensor = tf.image.convert_image_dtype(load(input_filename), tf.float32)
+    tensor = tf.image.convert_image_dtype(load(input_filename), dtype=tf.float32)
 
-    max_height = 1024
-    max_width = 1024
+    tensor = effects.square_crop_and_resize(tensor, 1024)
 
     with tf.Session().as_default():
         height, width, channels = tf.shape(tensor).eval()
 
-        need_resample = False
-
-        # Some presets only like square images. Work around for now by cropping.
-        if height != width:
-            length = min(height, width)
-            height = length
-            width = length
-
-            tensor = tf.image.resize_image_with_crop_or_pad(tensor, length, length)
-
-        if height > max_height:
-            need_resample = True
-            width = int(width * (max_height / height))
-            height = max_height
-
-        if width > max_width:
-            need_resample = True
-            height = int(height * (max_width / width))
-            width = max_width
-
-        shape = [height, width, channels]
-
-        if need_resample:
-            tensor = effects.resample(tensor, shape)
-
         kwargs, preset_name = presets.load(preset_name, presets.EFFECTS_PRESETS())
 
-        kwargs["shape"] = [height, width, channels]
+        kwargs["shape"] = [1024, 1024, channels]
 
         if "freq" not in kwargs:
             kwargs["freq"] = [3, 3]
