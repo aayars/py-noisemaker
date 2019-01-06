8466bdf3e6ec8bec663ec71c4bd484597e9b0639
Alex Ayars <aayars@gmail.com>
2019-01-06 21:52:04 +0000

    retro upscale

--- noisemaker/scripts/crop.py
+++ noisemaker/scripts/crop.py
@@ -13,10 +13,20 @@ import noisemaker.effects as effects
         https://github.com/aayars/py-noisemaker
         """, context_settings=cli.CLICK_CONTEXT_SETTINGS)
 @cli.name_option(default="cropped.jpg")
+@click.option('--retro-upscale', is_flag=True, help="Nearest-neighbor upsample (for small images)")
 @click.argument('input_filename')
 @click.pass_context
-def main(ctx, name, input_filename):
-    tensor = effects.square_crop_and_resize(tf.image.convert_image_dtype(load(input_filename), tf.float32), effects.shape_from_file(input_filename), 1024)
+def main(ctx, name, retro_upscale, input_filename):
+    shape = effects.shape_from_file(input_filename)
+
+    tensor = tf.image.convert_image_dtype(load(input_filename, channels=3), tf.float32)
+
+    if retro_upscale:
+        shape = [shape[0] * 2, shape[1] * 2, shape[2]]
+
+        tensor = effects.resample(tensor, shape, spline_order=0)
+
+    tensor = effects.square_crop_and_resize(tensor, shape, 1024)
 
     with tf.Session().as_default():
         save(tensor, name)
