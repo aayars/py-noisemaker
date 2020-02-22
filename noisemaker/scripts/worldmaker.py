3118c13b086ddd40dde05aadb61a626cc7b4d1fa
Alex Ayars <aayars@gmail.com>
2020-02-22 15:29:24 -0800

    worldmaker fixes

--- noisemaker/scripts/worldmaker.py
+++ noisemaker/scripts/worldmaker.py
@@ -5,17 +5,18 @@ import tensorflow as tf
 
 from noisemaker.util import save, load
 
+import noisemaker.constants as constants
 import noisemaker.effects as effects
 import noisemaker.generators as generators
 import noisemaker.recipes as recipes
 
-SMALL_X = 720
-SMALL_Y = 360
+SMALL_X = 512
+SMALL_Y = 256
 
-LARGE_X = 1440
-LARGE_Y = 720
+LARGE_X = 1024
+LARGE_Y = 512
 
-FREQ = 4
+FREQ = 3
 SATURATION = .333
 OCTAVES = 8
 
@@ -75,7 +76,7 @@ def midland():
         "octaves": OCTAVES,
         "point_freq": 5,
         "saturation": SATURATION * 2,
-        "voronoi_refract": .5,
+        "voronoi_refract": .25,
         "voronoi_alpha": .5,
         "voronoi_nth": 1,
         "with_voronoi": 6,
@@ -118,7 +119,7 @@ def highland():
 def _control():
     shape = [SMALL_Y, SMALL_X, 1]
 
-    control = generators.multires(shape=shape, freq=FREQ, octaves=OCTAVES, refract_range=1.0)
+    control = generators.multires(shape=shape, freq=FREQ, octaves=OCTAVES, refract_range=.5)
 
     erode_kwargs = {
         "alpha": .025,
@@ -130,7 +131,7 @@ def _control():
     iterations = 5
     for i in range(iterations):
         control = effects.erode(control, shape, **erode_kwargs)
-        control = effects.convolve(effects.ValueMap.conv2d_blur, control, shape)
+        control = effects.convolve(constants.ValueMask.conv2d_blur, control, shape)
 
     post_shape = [LARGE_Y, LARGE_X, 1]
     control = effects.resample(control, post_shape)
@@ -138,9 +139,9 @@ def _control():
     iterations = 2
     for i in range(iterations):
         control = effects.erode(control, post_shape, **erode_kwargs)
-        control = effects.convolve(effects.ValueMap.conv2d_blur, control, post_shape)
+        control = effects.convolve(constants.ValueMask.conv2d_blur, control, post_shape)
 
-    control = effects.convolve(effects.ValueMap.conv2d_sharpen, control, post_shape)
+    control = effects.convolve(constants.ValueMask.conv2d_sharpen, control, post_shape)
     control = effects.normalize(control)
 
     with tf.Session().as_default():
@@ -167,10 +168,10 @@ def blended():
     mid = tf.image.convert_image_dtype(load(MID_FILENAME), tf.float32)
     high = tf.image.convert_image_dtype(load(HIGH_FILENAME), tf.float32)
 
-    blend_control = generators.multires(shape=shape, freq=FREQ * 4, ridges=True, octaves=4)
-    blend_control = 1.0 - effects.value_map(blend_control, shape, keep_dims=True) * .5
+    # blend_control = generators.multires(shape=shape, freq=FREQ * 4, ridges=True, octaves=4)
+    # blend_control = 1.0 - effects.value_map(blend_control, shape, keep_dims=True) * .5
 
-    combined_land = effects.blend_layers(control, shape, blend_control, control * 2, low, mid, high)
+    combined_land = effects.blend_layers(control, shape, 1.0, low, low, mid, high, high)
     combined_land = effects.erode(combined_land, shape, xy_blend=.25, **erode_kwargs)
     combined_land = effects.erode(combined_land, shape, **erode_kwargs)
 
@@ -179,7 +180,7 @@ def blended():
 
     combined_land = effects.blend(combined_land_0, combined_land_1, .5)
 
-    combined = effects.blend_layers(control, shape, .01, water, combined_land, combined_land, combined_land)
+    combined = effects.blend_layers(control, shape, .01, water, combined_land, combined_land)
     combined = effects.blend(combined_land, combined, .625)
 
     combined = tf.image.adjust_brightness(combined, .1)
@@ -205,7 +206,7 @@ def clouds(input_filename):
         "ridges": True,
         "shape": pre_shape,
         "warp_freq": 3,
-        "warp_range": .25,
+        "warp_range": .125,
         "warp_octaves": 2,
     }
 
@@ -220,22 +221,23 @@ def clouds(input_filename):
     shadow = tf.minimum(shadow * 2.5, 1.0)
 
     for _ in range(3):
-        shadow = effects.convolve(effects.ValueMap.conv2d_blur, shadow, pre_shape)
+        shadow = effects.convolve(constants.ValueMask.conv2d_blur, shadow, pre_shape)
 
     shadow = effects.resample(shadow, post_shape)
     combined = effects.resample(combined, post_shape)
 
-    tensor = effects.blend(tensor, tf.zeros(post_shape), shadow * .5)
+    tensor = effects.blend(tensor, tf.zeros(post_shape), shadow * .75)
     tensor = effects.blend(tensor, tf.ones(post_shape), combined)
 
     post_shape = [LARGE_Y, LARGE_X, 3]
 
-    tensor = effects.shadow(tensor, post_shape, alpha=.25)
+    tensor = effects.shadow(tensor, post_shape, alpha=.75)
 
-    tensor = effects.bloom(tensor, post_shape, .333)
-    tensor = recipes.dither(tensor, post_shape, .075)
+    tensor = effects.bloom(tensor, post_shape, .5)
+    tensor = recipes.dither(tensor, post_shape, .125)
 
-    combined = tf.image.adjust_contrast(combined, 1.125)
+    tensor = tf.image.adjust_brightness(tensor, .125)
+    tensor = tf.image.adjust_contrast(tensor, 2.0)
 
     with tf.Session().as_default():
         save(tensor, FINAL_FILENAME)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='noisemaker',
-      version='0.1.171',
+      version='0.1.172',
       description='Generates procedural noise with Python 3 and TensorFlow',
       author='Alex Ayars',
       author_email='aayars@gmail.com',
