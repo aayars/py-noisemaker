e98ee6b5b579185c3c0c1992b11021c8991dc2c3
Alex Ayars <aayars@gmail.com>
2020-11-13 12:09:57 -0700

    use start variable

--- noisemaker/fastnoise.py
+++ noisemaker/fastnoise.py
@@ -27,7 +27,7 @@ def fastnoise(shape, freq, time=0.0, seed=None, speed=1.0, as_np=False):
     for channel in range(shape[2]):
         generator.seed = seed + 65536 * channel
 
-        tensor[:, :, channel] = tf.squeeze(generator.genAsGrid([shape[0], shape[1], 1], start=[0, 0, int(time * shape[0] * speed)]))
+        tensor[:, :, channel] = tf.squeeze(generator.genAsGrid([shape[0], shape[1], 1], start=[0, 0, start]))
 
     if not as_np:
         tensor = tf.stack(tensor)
