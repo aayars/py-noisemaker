c0bcb12015ba794c02109dc7e8824d1918f35565
Alex Ayars <aayars@gmail.com>
2025-09-21 06:53:12 -0600

    remove unused imports

--- noisemaker/presets.py
+++ noisemaker/presets.py
@@ -3,29 +3,8 @@ from functools import lru_cache
 from pathlib import Path
 import noisemaker.rng as random
 
-from noisemaker.composer import (
-    Effect,
-    Preset as ComposerPreset,
-    coin_flip,
-    enum_range,
-    random_member,
-    stash,
-)
-from noisemaker.constants import (
-    ColorSpace as color,
-    DistanceMetric as distance,
-    InterpolationType as interp,
-    OctaveBlending as blend,
-    PointDistribution as point,
-    ValueDistribution as distrib,
-    ValueMask as mask,
-    VoronoiDiagramType as voronoi,
-    WormBehavior as worms,
-)
-from noisemaker.palettes import PALETTES
+from noisemaker.composer import Preset as ComposerPreset
 from noisemaker.dsl import parse_preset_dsl
-
-import noisemaker.masks as masks
 
 @lru_cache(maxsize=1)
 def _cached_dsl_presets():
@@ -40,19 +19,16 @@ def _cached_dsl_presets():
         random.set_seed(seed_before)
 
 
-def _dsl_presets():
+def PRESETS():
     presets = deepcopy(_cached_dsl_presets())
 
-    # XXX Get rid of this
+    # This is somehow keeping the Python and JS ports in sync, removing it from both 
+    # places breaks parity. WTF
     random.random()
     random.random()
     random.random()
 
     return presets
-
-
-def PRESETS():
-    return _dsl_presets()
 
 
 def Preset(preset_name, *, settings=None):
