6b7077aa038f441ac4791ac7a6632fcd849a609d
Alex Ayars <aayars@gmail.com>
2025-11-09 10:38:31 -0700

    nuke broken shader

--- shaders/effects/sobel/effect.js
+++
@@ -1,6 +0,0 @@
-import SimpleComputeEffect from '../../common/simple-compute-effect.js';
-import meta from './meta.json' with { type: 'json' };
-
-export default class SobelEffect extends SimpleComputeEffect {
-  static metadata = meta;
-}
